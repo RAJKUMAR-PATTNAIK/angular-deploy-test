e59c25c9085ff695833b0cc1597256751e8eaab8
Rajkumar Pattnaik <rajkumarpattnaik@Rajkumars-MacBook-Pro.local>
2023-11-08 20:00:34 +0530

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "SecondCompComponent", component: SecondCompComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondCompComponent
   ],
   imports: [
     BrowserModule,
